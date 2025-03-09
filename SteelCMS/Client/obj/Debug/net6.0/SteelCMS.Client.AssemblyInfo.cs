2061b7b405a0fcdcc2b687b7bf3a5e24fdfd26ad
KKMM\USER <prawitchaya.kim@gmail.com>
2025-03-10 02:31:07 +0700

    no message

--- SteelCMS/Client/obj/Debug/net6.0/SteelCMS.Client.AssemblyInfo.cs
+++ SteelCMS/Client/obj/Debug/net6.0/SteelCMS.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SteelCMS.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4acaf358b31eac808428eb2f38991555d2196f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d54ddcf842b72f95b7d6240af7f0531f12c663d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SteelCMS.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SteelCMS.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
