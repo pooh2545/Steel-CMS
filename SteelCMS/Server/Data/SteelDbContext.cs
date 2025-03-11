c60787ec13b7f2b61555d8d1ac071c93ae41d49c
pooh2545 <poohpooh-pimpim@hotmail.com>
2025-03-12 03:06:05 +0700

    Add Dbset

--- SteelCMS/Server/Data/SteelDbContext.cs
+++ SteelCMS/Server/Data/SteelDbContext.cs
@@ -8,16 +8,6 @@ public class SteelDbContext : DbContext
     public SteelDbContext(DbContextOptions<SteelDbContext> options) : base(options) { }
 
     public DbSet<customers> customers { get; set; }
+    public DbSet<employee> employee { get; set; }
 }
 
-[Table("customers")]
-public class customers
-{
-    [Key] // กำหนดให้เป็น Primary Key
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // ✅ ทำให้เป็น Auto-Generated ID
-    public int row_Id { get; set; }
-    public string fullname { get; set; }  
-    public string email { get; set; }
-    public string password { get; set; }
-    public string? tel { get; set; }
-}
