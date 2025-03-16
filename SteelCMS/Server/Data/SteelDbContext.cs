01ed0bd740655513c7c9f84ab5010a10be452fe7
pooh2545 <poohpooh-pimpim@hotmail.com>
2025-03-16 17:46:03 +0700

    Dashboard Admin

--- SteelCMS/Server/Data/SteelDbContext.cs
+++ SteelCMS/Server/Data/SteelDbContext.cs
@@ -8,7 +8,6 @@ public class SteelDbContext : DbContext
     public SteelDbContext(DbContextOptions<SteelDbContext> options) : base(options) { }
 
     public DbSet<customers> customers { get; set; }
-    public DbSet<employee> employee { get; set; }
     public DbSet<steelproduct> steelproducts { get; set; }
     public DbSet<steeltype> steeltype { get; set; }
     public DbSet<order> orders { get; set; }
